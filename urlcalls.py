05f712d46fb19bbb2cea422ffdf0fd1b61ba20ad
David <dsj47@cornell.edu>
2023-06-17 16:48:18 -0700

    script to take in a prompt and spit out main webpage url

---
+++ urlcalls.py
@@ -0,0 +1,39 @@
+import openai
+import re
+from urllib.parse import urlparse
+
+openai.api_key = "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"
+
+
+def urls(request):
+    prompt = f"""give me the urls needed for a chrome: {request}"""
+    response = openai.ChatCompletion.create(
+        model="gpt-4-0613",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+        top_p=0.001,
+    )
+    # print(response["choices"][0]["message"]["content"])
+    return response["choices"][0]["message"]["content"]
+
+
+def clean_message(request):
+    pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    http_links = re.findall(pattern, urls(request))
+    return http_links
+
+
+def url_corr(urls_lst):
+    parsed_urls = [urlparse(url) for url in urls_lst]
+    netloc_set = set([parsed_url.netloc for parsed_url in parsed_urls])
+
+    if len(netloc_set) > 1:
+        print("Error: URLs are not from the same website.")
+        return Exception
+    else:
+        return urls_lst
+
+
+# print(clean_message("buy something on amazon"))
+print(url_corr(clean_message("buy a double aa battery on amazon")))
