241c53d99c606ab3deb996538871cdd1272526a2
Oluwapelumi Dada <91634699+Marvy101@users.noreply.github.com>
2023-06-18 13:36:55 -0700

    Update main.py
    Added ability to visit multiple pages

--- main.py
+++ main.py
@@ -8,98 +8,103 @@ import time
 import pyautogui
 import random
 import pygetwindow as gw
-from openaikey import openai
+from love import openai
 
 
-def browser_emulator(input_url, user_prompt):
+
+def browser_emulator(input_url, user_prompt, num_pages):
     options = Options()
     driver = webdriver.Chrome(options=options)
 
     driver.maximize_window()
     driver.get(input_url)
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.TAG_NAME, "a"))
-    )
+    for _ in range(num_pages):
 
-    links = driver.find_elements(By.CSS_SELECTOR, "a")
-    height = driver.get_window_size()["height"]
-    links = [
-        link
-        for link in links
-        if link.is_enabled() and link.is_displayed() and link.location["y"] <= height
-    ]
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.TAG_NAME, "a"))
+        )
 
-    link_data = []  # list to store link info
+        links = driver.find_elements(By.CSS_SELECTOR, "a")
+        height = driver.get_window_size()["height"]
+        links = [link for link in links if link.is_enabled() and link.is_displayed() and link.location["y"] <= height]
 
-    for link in links:
-        # collect href, text, size, and location
-        link_info = {
-            "href": link.get_attribute("href"),
-            "text": link.text,
-            "size": link.size,
-            "location": link.location,
-        }
-        link_data.append(link_info)
+        link_data = []  # list to store link info
 
-    text = []
-    for i in range(len(link_data)):
-        text.append(link_data[i]["text"])
-    print(text)
+        for link in links:
+            link_info = {
+                "href": link.get_attribute("href"),
+                "text": link.text,
+                "size": link.size,
+                "location": link.location,
+            }
+            link_data.append(link_info)
 
-    # Use GPT-4 to decide which link to click
-    link_texts = [link["text"] for link in link_data]
-    link_prompt = (
-        f"{user_prompt}\n All these texts are buttons on a webpage, output the most relevant one to the User Prompt. Do not output anything else/ \n \nOptions:\n"
-        + "\n".join(link_texts)
-    )
-    response = openai.ChatCompletion.create(
-        model="gpt-4-0613",
-        messages=[
-            {"role": "system", "content": "You are a brilliant comedian"},
-            {"role": "user", "content": link_prompt},
-        ],
-    )
+        # Use GPT-4 to decide which link to click
+        link_texts = [link["text"] for link in link_data]
+        print("List of texts to make decision from: ", link_texts)
+        link_prompt = f"{user_prompt}\n All these texts are buttons on a webpage, output the most relevant one to the User Prompt. Do not output anything else/ \n \nOptions:\n" + "\n".join(link_texts)
+        response = openai.ChatCompletion.create(
+            model="gpt-4-0613",
+            messages=[
+                {"role": "system", "content": "You are a brilliant comedian"},
+                {"role": "user", "content": link_prompt},
+            ],
+        )
 
-    chosen_link_text = response["choices"][0]["message"]["content"]
-    chosen_link_info = next(
-        (link for link in link_data if link["text"] == chosen_link_text), None
-    )
+        chosen_link_text = response["choices"][0]["message"]["content"]
+        chosen_link_info = next((link for link in link_data if link["text"] == chosen_link_text), None)
 
-    print("Chosen Link text: ", chosen_link_text)
-    print("Chosen Link Info: ", chosen_link_info)
+        if not chosen_link_info:
+            print("GPT-4 did not select a valid link. Please try again.")
+            return
 
-    if not chosen_link_info:
-        print("GPT-4 did not select a valid link. Please try again.")
-        return
+        browser_info = gw.getWindowsWithTitle(driver.title)
+        print("\n Choice:", chosen_link_text)
 
-    browser_window = gw.getWindowsWithTitle(driver.title)[0]
-    window_position = browser_window.topleft
-    # window_position_x = window_position[0]
-    # window_position_y = window_position[1]
+        browser_window = gw.getWindowsWithTitle(driver.title)[0]
+        window_position = browser_window.topleft
+        window_position_x = window_position[0]
+        window_position_y = window_position[1]
 
-    # Assuming an offset of 100 pixels for the URL bar
-    offset_y = 162
+        # Assuming an offset of 100 pixels for the URL bar
+        offset_y = 116
 
-    link_size = chosen_link_info["size"]
-    link_location = chosen_link_info["location"]
+        link_size = chosen_link_info['size']
+        link_location = chosen_link_info['location']
 
-    center_x = link_location["x"]
-    center_y = link_location["y"]
+        center_x = link_location['x'] + link_size['width'] / 2
+        center_y = link_location['y'] + link_size['height'] / 2
 
-    screen_x = center_x
-    screen_y = center_y + offset_y
+        screen_x = window_position_x + center_x
+        screen_y = window_position_y + center_y + offset_y
 
-    pyautogui.moveTo(screen_x, screen_y, duration=0.5)
-    pyautogui.click(x=screen_x, y=screen_y)
+        pyautogui.moveTo(screen_x, screen_y, duration=0.5)
+        pyautogui.click(x=screen_x, y=screen_y)
+
+
 
     time.sleep(5)
     driver.quit()
 
+# Example user_prompt
+user_prompt = "Buy iphone"
+url = "https://www.apple.com/"
 
-# Let's give an example of user_prompt, but it should come from your actual user input
+# Define the GPT-4 interaction to obtain number of pages to navigate
+prompt = f'''You are an AI that visits websites how many pages will it take you to accomplish: user_prompt = {user_prompt} from this url:{url}. Output only a number without saying any other thing.'''
+response = openai.ChatCompletion.create(
+    model="gpt-4-0613",
+    messages=[
+        {"role": "system", "content": "You are only output numbers"},
+        {"role": "user", "content": prompt},
+    ],
+)
 
-# user_prompt = "cart"
-# url = "https://github.com/pywinauto/pywinauto"
+# Extract the number of pages from the response
+num_pages = int(response["choices"][0]["message"]["content"])
 
-# browser_emulator(url, user_prompt)
+print(num_pages)
+
+# Call the function with url, user_prompt, and the obtained num_pages
+browser_emulator(url, user_prompt, num_pages)
